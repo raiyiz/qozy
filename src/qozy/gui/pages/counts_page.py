db2e2c79413db6e3dcd6b4ee2893a3162c96adde
ↄǝsƃ <uosod@n>
2026-09-04 18:37:51 +0200

    fixup! linting

--- src/qozy/gui/pages/counts_page.py
+++ src/qozy/gui/pages/counts_page.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 import numpy as np
-from PyQt6.QtCore import QMetaObject, Qt, pyqtSignal
+from PyQt6.QtCore import pyqtSignal
 from PyQt6.QtWidgets import (
     QCheckBox,
     QFormLayout,
